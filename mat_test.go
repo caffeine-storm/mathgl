907a45ddc9b9fc3440eadb371bcad14bd8d8bc3d
Thomas McKee <caffeine.storm@gmail.com>
2025-02-28 11:37:13 -0500

    test: mat3 should be able to translate too
    tdd: red

--- mat_test.go
+++ mat_test.go
@@ -8,15 +8,32 @@ import (
 )
 
 func TestTranslation(t *testing.T) {
-	var m mathgl.Mat4
-	m.Translation(4, 7, 0)
+	t.Run("mat3", func(t *testing.T) {
+		var m mathgl.Mat3
+		m.Translation(4, 7)
 
-	// Column major order, remember!
-	expected := []float32{
-		1, 0, 0, 4,
-		0, 1, 0, 7,
-		0, 0, 1, 0,
-		0, 0, 0, 1,
-	}
-	assert.Equal(t, mathgl.Mat4(expected), m)
+		// Column major order, remember!
+		expected := []float32{
+			1, 0, 4,
+			0, 1, 7,
+			0, 0, 1,
+		}
+
+		assert.Equal(t, mathgl.Mat3(expected), m)
+	})
+
+	t.Run("mat4", func(t *testing.T) {
+		var m mathgl.Mat4
+		m.Translation(4, 7, 0)
+
+		// Column major order, remember!
+		expected := []float32{
+			1, 0, 0, 4,
+			0, 1, 0, 7,
+			0, 0, 1, 0,
+			0, 0, 0, 1,
+		}
+
+		assert.Equal(t, mathgl.Mat4(expected), m)
+	})
 }
